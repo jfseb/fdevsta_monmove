b8b49cb6627af7af52c9a5e0479afff1c5e24054
jfseb <jfsebpublic@gmail.com>
2017-03-11 20:09:08 +0100

    tests cleanup

--- js/match/rule.d.ts
+++ js/match/rule.d.ts
@@ -1,38 +1,5 @@
 import * as IMatch from './ifmatch';
 export declare const oKeyOrder: Array<String>;
-export declare function getRuleMap(): {
-    "systemObjectCategory": {
-        type: IMatch.EnumRuleType;
-        key: string;
-        word: string;
-        follows: {
-            systemObjectCategory: string;
-        };
-    }[];
-    "systemId": ({
-        regexp: RegExp;
-        key: string;
-        argsMap: {
-            1: string;
-            2: string;
-        };
-        type: IMatch.EnumRuleType;
-        follows: {};
-    } | {
-        regexp: RegExp;
-        key: string;
-        argsMap: {
-            1: string;
-        };
-        type: IMatch.EnumRuleType;
-        follows: {};
-    })[];
-};
 export declare function compareMRuleFull(a: IMatch.mRule, b: IMatch.mRule): number;
 export declare function cmpMRule(a: IMatch.mRule, b: IMatch.mRule): number;
-export declare function getIntMRulesSample(): Array<IMatch.mRule>;
-export declare function getMRulesSample(): IMatch.SplitRules;
 export declare function assureLowerCaseWord(mRules: Array<IMatch.mRule>): IMatch.mRule[];
-export declare function getUnitTestUrl(string: string): any;
-export declare function getWikiUrl(string: string): any;
-export declare function getMRulesFull(): IMatch.SplitRules;

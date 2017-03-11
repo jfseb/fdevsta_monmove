806eeab4cca522eb0d5cda6feed5530e82108544
jfseb <jfsebpublic@gmail.com>
2017-03-11 02:05:46 +0100

    push

--- js/index.js
+++ js/index.js
@@ -9,4 +9,5 @@ __export(require("./model/meta"));
 __export(require("./match/ifmatch"));
 // Sample export of our own
 exports.bas = "123";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxvQ0FBb0M7QUFDcEMsbUNBQThCO0FBQzlCLGtDQUE2QjtBQUM3QixxQ0FBZ0M7QUFFaEMsMkJBQTJCO0FBQ2hCLFFBQUEsR0FBRyxHQUFHLEtBQUssQ0FBQyJ9
+
+//# sourceMappingURL=index.js.map
